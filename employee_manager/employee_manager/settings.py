d98ed30100686c1ad9fb8cc52e86cd081e6e3c18
Michael Gomes da Silva <48489202+MichaelGomes98@users.noreply.github.com>
2022-02-04 17:57:51 +0100

    Nettoyage du code

--- employee_manager/employee_manager/settings.py
+++ employee_manager/employee_manager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yt2&5o8d7c3%=dnyjcg&q6)v4brpno@#&p807x+7inhh$+g3t^'
+SECRET_KEY = 'XXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
